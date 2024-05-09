f90ec48fefa73fd270ecdaf821d0986d4298068b
Vision-Scripts <167629435+MacSyst@users.noreply.github.com>
2024-05-10 00:52:55 +0200

    Add files via upload

---
+++ vision_hudv2/server/server.lua
@@ -0,0 +1,33 @@
+-- Made By Benni (Vision-Service) with <3 for the Community.
+
+ESX = exports["es_extended"]:getSharedObject()
+
+local xPlayer = ESX.GetPlayerFromId(source)
+
+ESX.RegisterServerCallback('vision_hudv2:getaccounts', function(src, cb)
+	local xPlayer = ESX.GetPlayerFromId(src)
+  local accounts = {
+    ['bank'] = xPlayer.getAccount('bank').money,
+    ['money'] = xPlayer.getMoney()
+  }
+  cb(accounts)
+end)
+
+
+
+
+
+ESX = exports["es_extended"]:getSharedObject()
+
+function ShowPlayers()
+	local xPlayers = ESX.GetPlayers()
+	PlayersOnline = 0
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+        PlayersOnline = PlayersOnline + 1
+	end
+    TriggerClientEvent('vision_hud:setPlayers', -1, PlayersOnline)
+    SetTimeout(5000, ShowPlayers)
+end
+
+ShowPlayers()
